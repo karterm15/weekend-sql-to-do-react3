90b610edcb96b5678cc520e0c216989013d8f178
karterm15 <kartermoua151@gmail.com>
2024-12-05 17:35:25 -0700

    Initial commit

---
+++ server/routes/todo.router.js
@@ -0,0 +1,57 @@
+const express = require('express');
+const router = express.Router();
+const pool = require('../modules/pool.js');
+
+// GET
+router.get( '/', (req, res)=>{
+    console.log( '/api/todo GET' );
+    const queryString = `SELECT * from todo;`;
+    pool.query( queryString ).then( (results) =>{
+        res.send( results.rows );
+    }).catch( ( err )=>{
+        console.log ( err );
+        res.sendStatus( 400 );
+    })
+})
+
+// POST
+router.post( '/', (req,res )=>{
+    console.log( '/api/todo POST:', req.body, req.query)
+    const queryString = `INSERT INTO "todo" ("name", "complete") VALUES ( $1, false);`;
+    const values = [ req.body.name];
+    pool.query( queryString, values ).then( ( results)=>{
+        res.sendStatus(201);
+    }).catch( ( err )=>{
+        console.log( err );
+        res.sendStatus( 400 );
+    })
+})
+
+// PUT
+router.put( '/', ( req, res)=>{
+    console.log( '/api/todo PUT hit:', req.body );
+    const queryString = `UPDATE "todo" SET complete =NOT complete WHERE "id"=$1;`;
+    const values = [ req.body.id ];
+    pool.query( queryString, values ).then( ( results )=>{
+        res.sendStatus( 200 );
+    }).catch( ( err )=>{
+        console.log( err );
+        res.sendStatus( 400 );
+    })
+})
+
+// DELETE
+router.delete('/', (req, res)=>{
+    console.log( '/api/todo DELETE:', req.body, req.query );
+    const queryString = `DELETE FROM "todo" WHERE "id"=$1;`;
+    const values = [ req.query.id ];
+    pool.query( queryString, values ).then ( ( results )=>{
+        res.sendStatus( 200);
+    }).catch( ( err )=>{
+        console.log( err );
+        res.sendStatus( 400 );
+    })
+   
+})
+
+module.exports = router;
---
+++ src/components/App/App.jsx
@@ -0,0 +1,87 @@
+import {useState, useEffect} from 'react';
+import axios from 'axios';
+import './App.css';
+
+
+function App () {
+ const[ todoList, setTodoList ] = useState ([]);  //Need an array that can be put on dom for todo list(use hook)
+ const [ newItemName, setNewItemName] = useState ( '' )
+
+ useEffect( ()=>{
+  fetchTodoList()
+ }, [] );
+
+ function addItem(){
+  const objectToSend = {
+    name: newItemName
+  }
+  console.log( 'sending', objectToSend );
+  axios.post( '/api/todo', objectToSend ).then(function ( response ){
+    console.log( 'back from POST:', response.data);
+    fetchTodoList();
+  }).catch(function( err ){
+    console.log( err);
+    alert(' error adding itme');
+  })
+ }
+
+ function deleteMe( id ){
+ console.log( 'in deleteMe:', id );
+ axios.delete( `/api/todo?id=${ id }`).then( function( response){
+  console.log( 'back from delete:', response.data);
+  fetchTodoList();
+ }).catch( function( err ){
+  console.log( err );
+  alert( 'error deleting item' );
+ })
+ }
+
+
+
+ function fetchTodoList(){
+    console.log('in fetchTodoList' );
+    axios.get( '/api/todo').then( function( response){
+      setTodoList( response.data );
+    }).catch(function( err ){
+      console.log( err );
+      alert( 'error getting todo list' );
+    })
+ }
+
+function toggleMe( id ){
+  console.log( ' in toggleMe', id );
+  const objectToSend = {
+    id: id
+  }
+  axios.put( '/api/todo', objectToSend ).then( function( response ){
+    console.log( ' back from PUT:', response.data );
+    fetchTodoList();
+  }).catch( function( err ){
+    console.log( err );
+    alert( 'error updating todo item' );
+  })
+}
+
+
+  return (
+    <div>
+      <h1>TO DO APP</h1>
+      <h2>Current List:</h2>
+      {
+        todoList.map( (item)=>(
+          <p className={ `complete-${item.complete}` } key={ item.id } >{ item.name } 
+          <button onClick={ ()=>{ toggleMe(item.id) }}>Toggle Complete</button> 
+          <button onClick={ ()=>{ deleteMe(item.id) }}>Delete</button>
+          </p>
+        ))
+      }
+      <h2> Creat New:</h2>
+      <input type="text" placeholder='Task'onChange={ (e)=>{ setNewItemName( e.target.value ) }} />
+      <button onClick={addItem}>Add Item</button>
+      <p>{JSON.stringify( newItemName )}</p>
+    </div>
+  );
+
+}
+
+export default App
